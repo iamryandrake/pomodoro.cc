e581093c7c6880e502ac8f4a5751f8284855ae78
Christian Fei <crifei93@gmail.com>
2015-05-19 20:52:30 +0200

    added logs to socket-io

--- socket-io/modules/SocketIOBehaviour.js
+++ socket-io/modules/SocketIOBehaviour.js
@@ -1,8 +1,11 @@
 module.exports = function SocketIOBehaviour(rooms){
   return function(socket){
+
+
     var room = ''
     socket.on('room', function(_room){
       if( !_room )return
+      console.log('-- entering room: ', _room)
       room = _room
       rooms.addTo(room)
       socket.join(room)
@@ -14,18 +17,23 @@ module.exports = function SocketIOBehaviour(rooms){
       socket.emit('clientsCount', clientsCount)
     })
     socket.on('pomodoroSyncResponse', function(remainingTime){
+      console.log('-- pomodoroSyncResponse: ', remainingTime)
       socket.broadcast.to(room).emit('pomodoroSyncResponse', remainingTime)
     })
     socket.on('start', function(minutes,type){
+      console.log('-- start: ', minutes, type)
       socket.broadcast.to(room).emit('start',minutes,type)
     })
     socket.on('stop', function(minutes,type){
+      console.log('-- stop: ', minutes, type)
       socket.broadcast.to(room).emit('stop',minutes,type)
     })
     socket.on('event', function(obj){
+      console.log('-- event: ', obj)
       socket.broadcast.to(room).emit('event',obj)
     })
     socket.on('disconnect', function(){
+      console.log('-- disconnect')
       rooms.removeFrom(room)
       var clientsCount = rooms.countFor(room)
       socket.broadcast.to(room).emit('clientsCount', clientsCount)
