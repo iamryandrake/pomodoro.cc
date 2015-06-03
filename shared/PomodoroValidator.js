127303708a829ff5496000b81f2d15b20f5f3e0b
Christian Fei <crifei93@gmail.com>
2015-06-03 08:18:21 +0200

    added test for shared modules

---
+++ shared/PomodoroValidator.js
@@ -0,0 +1,19 @@
+module.exports = {
+  isValid: isValid,
+  validate: validate
+}
+
+function isValid(pomodoro){
+  var errors = validate(pomodoro)
+  if( Object.keys(errors).length > 0 )
+    return false
+  return true
+}
+
+function validate(pomodoro){
+  var errors = {}
+  if( Object.keys(pomodoro).length === 0 ){
+    errors.type = 'required'
+  }
+  return errors
+}
---
+++ shared/PomodoroValidator.test.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect
+var PomodoroValidator = require('./PomodoroValidator')
+
+describe('PomodoroValidator', function () {
+  it('validates a Pomodoro', function () {
+    var isValid = PomodoroValidator.isValid({})
+    expect( isValid ).to.be.false
+  })
+  it('returns errors for Pomodoro', function () {
+    var errors = PomodoroValidator.validate({})
+    expect(errors).to.deep.equal({
+      type: 'required'
+    })
+  })
+})
