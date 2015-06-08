5cac36c0872612ff7c87155adce0fe71a8d4fc34
Christian Fei <crifei93@gmail.com>
2015-06-08 08:37:55 +0200

    docker.building relative directories to project

--- opt/docker.build.sh
+++ opt/docker.build.sh
@@ -2,12 +2,12 @@
 
 echo "\n\n"
 echo "----> building pomodoro"
-docker build -t christianfei/pomodoro-app /pomodoro.cc/app
+docker build -t christianfei/pomodoro-app app
 
 echo "\n\n"
 echo "----> building pomodoro-api"
-docker build -t christianfei/pomodoro-api /pomodoro.cc/api
+docker build -t christianfei/pomodoro-api api
 
 echo "\n\n"
 echo "----> building pomodoro-socket-io"
-docker build -t christianfei/pomodoro-socket-io /pomodoro.cc/socket-io
+docker build -t christianfei/pomodoro-socket-io socket-io
