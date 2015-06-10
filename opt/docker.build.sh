f577aa75214a5bdef92bdb470e2844686b38f83e
Christian Fei <crifei93@gmail.com>
2015-06-11 00:03:26 +0200

    relative location for dockerfile locations in docker.build

--- opt/docker.build.sh
+++ opt/docker.build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname `readlink -f $0`)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+
+if [ "$PROJECT_DIR" == "/" ]; then
+  PROJECT_DIR="/pomodoro.cc"
+fi
+
 echo "\n\n"
 echo "----> building pomodoro"
-docker build -t christianfei/pomodoro-app app
+docker build -t christianfei/pomodoro-app $PROJECT_DIR/app
 
 echo "\n\n"
 echo "----> building pomodoro-api"
-docker build -t christianfei/pomodoro-api api
+docker build -t christianfei/pomodoro-api $PROJECT_DIR/api
 
 echo "\n\n"
 echo "----> building pomodoro-socket-io"
-docker build -t christianfei/pomodoro-socket-io socket-io
+docker build -t christianfei/pomodoro-socket-io $PROJECT_DIR/socket-io
