1cd4f8509dda0dbebc9b4502c50824b07cc70d87
Christian Fei <christian.fei@xpeppers.com>
2015-07-13 18:05:35 +0200

    added test for NumberUtils

--- shared/NumberUtils.test.js
+++ shared/NumberUtils.test.js
@@ -4,5 +4,9 @@ var expect = require('chai').expect
 describe('NumberUtils', function() {
   describe('limitDecimals', function () {
     expect( NumberUtils.limitDecimals(10.0000) ).to.eql( 10.00 )
+    expect( NumberUtils.limitDecimals(10.0000, 2) ).to.eql( 10.00 )
+    expect( NumberUtils.limitDecimals(-10.123) ).to.eql( -10.12 )
+    expect( NumberUtils.limitDecimals(-10.123, 2) ).to.eql( -10.12 )
+    expect( NumberUtils.limitDecimals(-10) ).to.eql( -10 )
   })
 })
