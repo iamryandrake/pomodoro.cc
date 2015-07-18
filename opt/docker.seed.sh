54e46236f8d4149a04e5ad6a3387d86f6d4a1858
Christian Fei <crifei93@gmail.com>
2015-07-18 17:20:05 +0200

    testing if seed on circle is fixed added relative path to seed script

--- opt/docker.seed.sh
+++ opt/docker.seed.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --rm -it --link pomodoro-api-db:mongo_alias -v /pomodoro.cc/api/opt/mongo/seed.js:/run.js mongo sh -c "mongo --host mongo_alias < /run.js"
+SCRIPT_DIR=$(dirname `readlink -f $0`)
+PROJECT_DIR=$(dirname $SCRIPT_DIR)
+if [ "$PROJECT_DIR" = "/" ]; then
+  PROJECT_DIR="/pomodoro.cc"
+fi
+
+docker run --rm -it --link pomodoro-api-db:mongo_alias -v $PROJECT_DIR/api/opt/mongo/seed.js:/run.js mongo sh -c "mongo --host mongo_alias < /run.js"
