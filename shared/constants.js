98a8a840c77fbe011eef6ce9defeab55c1234b20
Christian Fei <crifei93@gmail.com>
2015-09-14 20:35:17 +0200

    seo impr

--- shared/constants.js
+++ shared/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   dayFormat: 'MM/DD/YYYY',
   weekFormat: 'W/GGGG',
-  title: 'Pomodoro.cc - Time tracking with the Pomodoro technique',
+  title: 'Pomodoro.cc - Time tracking with the Pomodoro technique - Online Timer',
   tagsLimit:5,
 }
